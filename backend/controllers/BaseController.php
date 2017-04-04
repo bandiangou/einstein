66c4f5e42ed63b7d147dbadb89fd6f0ec979e85e
ironwaterman <jackdeng@126.com>
2017-04-04 19:18:23 +0800

    add login controller

---
+++ backend/controllers/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: underwood
+ * Date: 2017-4-4
+ * Time: 07:16 PM
+ */
+
+namespace backend\controllers;
+
+use yii\web\Controller;
+
+class BaseController extends Controller{
+
+    public function actionLogin(){
+
+    }
+
+    public function actionLogout(){
+        
+    }
+
+}
